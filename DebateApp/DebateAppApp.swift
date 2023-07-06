f022673bb9c008803bb312b1f1370b3f2b1e0ec5
Datta Kurapati <kaushik25211@gmail.com>
2023-07-06 14:47:14 -0500

    Initial Commit

---
+++ DebateApp/DebateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebateAppApp.swift
+//  DebateApp
+//
+//  Created by Kaushik Kurapati on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DebateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
