a9369c09a5e859133a1ba26a0d3c299c9349495d
Datta Kurapati <kaushik25211@gmail.com>
2023-07-08 02:32:48 -0500

    Tried some stuff, probably going to switch to a side menu

--- DebateApp/ContentView.swift
+++ DebateApp/ContentView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let debates = debateEvents
+    let debateView = debateViews
+    let speeches = speechEvents
+    
     var body: some View {
-        Home()
+        //        Home()
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(debates, id: \.self) { debateEvent in
+                        NavigationLink(destination: debateView[0]) {
+                            Image(systemName: "airplane")
+                            Text(debateEvent)
+                        }.padding()
+                        
+                    } .navigationTitle("Debate Events")
+                }
+            }
+        }
     }
 }
 
---
+++ DebateApp/Events.swift
@@ -0,0 +1,28 @@
+//
+//  Events.swift
+//  DebateApp
+//
+//  Created by Kaushik Kurapati on 7/8/23.
+//
+
+import Foundation
+
+let debateEvents = [
+    "Lincoln Douglas",
+    "Cross Examination/Policy",
+    "Public Forum",
+    "World Schools",
+    "Extemperaneous",
+    ]
+
+let speechEvents = [
+    "Oringal Oratory",
+    "Impromptu",
+    "Extemperaneous Commentary",
+    "IX",
+    "DX",
+    "Poetry",
+    "Storytelling"]
+
+let debateViews = [
+ Home()]
