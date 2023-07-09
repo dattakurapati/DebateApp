77f0109988650b3ec116bb376d0793a6bb6f6622
Datta Kurapati <kaushik25211@gmail.com>
2023-07-08 19:54:20 -0500

    Added the side panel the ability to switch between pages.

--- DebateApp/ContentView.swift
+++ DebateApp/ContentView.swift
@@ -12,24 +12,76 @@ struct ContentView: View {
     let debates = debateEvents
     let debateView = debateViews
     let speeches = speechEvents
+      
+    @State private var showMenu: Bool = false
     
     var body: some View {
-        //        Home()
+        // Home()
         NavigationView {
-            VStack {
-                List {
-                    ForEach(debates, id: \.self) { debateEvent in
-                        NavigationLink(destination: debateView[0]) {
-                            Image(systemName: "airplane")
-                            Text(debateEvent)
-                        }.padding()
-                        
-                    } .navigationTitle("Debate Events")
+              
+              ZStack {
+                
+                Color.mint.ignoresSafeArea(.all, edges: .all)
+                
+                VStack {
+                  Text("ContentView")
+                    .padding()
+                    .font(.title)
+                    .foregroundColor(.white)
                 }
-            }
-        }
+                
+                GeometryReader { _ in
+                  
+                  HStack {
+                    Spacer()
+                    
+                    SideMenuView()
+                      .offset(x: showMenu ? 0 : UIScreen.main.bounds.width)
+                      .animation(.easeInOut(duration: 0.4), value: showMenu)
+                  }
+                  
+                }
+                .background(Color.black.opacity(showMenu ? 0.5 : 0))
+                
+              }
+              .navigationTitle("Home")
+              .navigationBarTitleDisplayMode(.inline)
+              .toolbar {
+                
+                Button {
+                  self.showMenu.toggle()
+                } label: {
+                  
+                  if showMenu {
+                    
+                    Image(systemName: "xmark")
+                      .font(.title)
+                      .foregroundColor(.red)
+                    
+                  } else {
+                    Image(systemName: "text.justify")
+                      .font(.title)
+                      .foregroundColor(.red)
+                  }
+                  
+                }
+              }
+        }.navigationBarBackButtonHidden(true)
+          }
+//        NavigationView {
+//            VStack {
+//                List {
+//                    ForEach(debates, id: \.self) { debateEvent in
+//                        NavigationLink(destination: debateView[0]) {
+//                            Image(systemName: "airplane")
+//                            Text(debateEvent)
+//                        }.padding()
+//
+//                    } .navigationTitle("Debate Events")
+//                }
+//            }
+//        }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- DebateApp/View/Home.swift
+++ DebateApp/View/Home.swift
@@ -9,46 +9,88 @@ import SwiftUI
 
 
 struct Home: View {
+    
+    @State private var showMenu: Bool = false
+    
     var body: some View {
+        
         NavigationView {
-            ScrollView {
-                VStack {
-                    Divider()
-                    Text("Example of an LD Debate")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .padding(.leading, 50)
-                        .padding(.trailing, 50)
-                    
-                    YTView(ID: "Dw6Nvv8UMbU")
-                    
-                    Text("Example of an LD Debate")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .padding(.leading, 50)
-                        .padding(.trailing, 50)
-                    
-                    YTView(ID: "Dw6Nvv8UMbU")
+            ZStack {
                 
-                    Text("Example of an LD Debate")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .padding(.leading, 50)
-                        .padding(.trailing, 50)
-                    
-                    YTView(ID: "Dw6Nvv8UMbU")
+                ScrollView {
+                    VStack {
+                        Divider()
+                        Text("Example of an LD Debate")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .padding(.leading, 50)
+                            .padding(.trailing, 50)
+                        
+                        YTView(ID: "Dw6Nvv8UMbU")
+                        
+                        Text("Example of an LD Debate")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .padding(.leading, 50)
+                            .padding(.trailing, 50)
+                        
+                        YTView(ID: "Dw6Nvv8UMbU")
+                        
+                        Text("Example of an LD Debate")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .padding(.leading, 50)
+                            .padding(.trailing, 50)
+                        
+                        YTView(ID: "Dw6Nvv8UMbU")
+                        
+                        Text("Example of an LD Debate")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .padding(.leading, 50)
+                            .padding(.trailing, 50)
+                        
+                        YTView(ID: "Dw6Nvv8UMbU")
+                    }
+                }
                 
-                    Text("Example of an LD Debate")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                        .padding(.leading, 50)
-                        .padding(.trailing, 50)
+                GeometryReader { _ in
+                  
+                  HStack {
+                    Spacer()
                     
-                    YTView(ID: "Dw6Nvv8UMbU")
+                    SideMenuView()
+                      .offset(x: showMenu ? 0 : UIScreen.main.bounds.width)
+                      .animation(.easeInOut(duration: 0.4), value: showMenu)
+                  }
+                  
+                }
+                .background(Color.black.opacity(showMenu ? 0.5 : 0))
+
+            }
+            .navigationTitle("Lincoln Douglas")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                
+                Button {
+                    self.showMenu.toggle()
+                } label: {
+                    
+                    if showMenu {
+                        
+                        Image(systemName: "xmark")
+                            .font(.title)
+                            .foregroundColor(.red)
+                        
+                    } else {
+                        Image(systemName: "text.justify")
+                            .font(.title)
+                            .foregroundColor(.red)
+                    }
+                    
                 }
             }
-            .navigationTitle("LD Debate")
-        }
+        }.navigationBarBackButtonHidden(true)
     }
 }
 
